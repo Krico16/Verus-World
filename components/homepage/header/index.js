3120894054f8ab2a3809de8daae213ad1fda331e
Angel Castillo <bash_stone_u@hotmail.com>
2021-09-30 20:56:18 -0500

    Fixed Overlay

--- components/homepage/header/index.js
+++ components/homepage/header/index.js
@@ -5,7 +5,7 @@ import TopLinks from "./topLinks";
 
 const HomePage = ({info,meta}) => {
     return (
-        <div id="top" className="bged overlay" style={{backgroundImage:`url(https://images.unsplash.com/photo-1437413646517-e98caade5a58?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80)`}}>
+        <div id="top" className="bgded overlay" style={{backgroundImage:`url(https://images.unsplash.com/photo-1437413646517-e98caade5a58?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80)`}}>
             <TopArea>
                 <Logo info={info} />
                 <TopLinks />
