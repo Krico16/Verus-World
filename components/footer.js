9452e9c46f87dd2e503ad0ac3ccc6bcbb0898309
Angel Castillo <bash_stone_u@hotmail.com>
2021-10-17 18:06:01 -0500

    Changed Copy section Changed button size

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
                 <div className="row">
                     <div className="col">
                         <div className="color-white text-center" data-wow-delay=".4s">
-                            <p style={{'fontSize':'2.5vh !important'}}>Diseñado y hecho con ❤ por <a href="https://github.com/Krico16">Krico</a> </p>
+                            <p style={{'fontSize':'1rem !important'}}>Diseñado y hecho con ❤ por <a href="https://github.com/Krico16">Krico</a> </p>
                         </div>
                     </div>
                 </div>
